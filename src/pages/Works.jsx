5169cee7b3de056df473b5281300b0a55f5d827a
R5Z Yoon <asaucybloom@gmail.com>
2026-04-29 15:56:01 +0900

    style: add animations to works page

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -5,11 +5,15 @@ const Works = () => {
 
   return (
     <div className="works-container">
+      {/* 떠다니는 원들 */}
+      <div className="floating-circle circle-1"></div>
+      <div className="floating-circle circle-2"></div>
+      <div className="floating-circle circle-3"></div>
+      
       <div className="works-grid">
         {gridItems.map((item) => (
           <div key={item} className="work-item">
             <div className="work-placeholder">
-              <span className="coming-soon"></span>
             </div>
           </div>
         ))}
